7799fc0b9d04e288ce852ebe4d1018e2d018e637
montaniaRoja <adolfocp1972@gmail.com>
2025-01-21 16:21:44 -0600

    se cambio la conexion

--- vms/admin/include/config.php
+++ vms/admin/include/config.php
@@ -1,15 +1,27 @@
-?php
+<?php
 $ip = $_SERVER['REMOTE_ADDR'];
 $ip_locales = array('127.0.0.1', '::1');
 $is_localhost = in_array($ip, $ip_locales);
+
 if ($is_localhost) {
+    // Para entorno local
     $db = pg_connect("host=localhost port=5432 dbname=tuvet user=adolfo password=ofloda01");
 } else {
+    // Para entorno remoto (Google Cloud SQL o IP pública)
     $host = getenv('PGHOST');
     $port = getenv('PGPORT');
     $dbname = getenv('PGDATABASE');
     $user = getenv('PGUSER');
     $password = getenv('PGPASSWORD');
+
+    if (!$host || !$port || !$dbname || !$user || !$password) {
+        die('Faltan variables de entorno');
+    }
+
     $db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+    if (!$db) {
+        die('Error de conexión: ' . pg_last_error());
+    }
 }
 ?>
--- vms/include/config.php
+++ vms/include/config.php
@@ -1,34 +1,28 @@
 <?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$ip_locales = array('127.0.0.1', '::1');
+$is_localhost = in_array($ip, $ip_locales);
 
+if ($is_localhost) {
+    // Para entorno local
+    $db = pg_connect("host=localhost port=5432 dbname=tuvet user=adolfo password=ofloda01");
+} else {
+    // Para entorno remoto (Google Cloud SQL o IP pública)
+    $host = getenv('PGHOST');
+    $port = getenv('PGPORT');
+    $dbname = getenv('PGDATABASE');
+    $user = getenv('PGUSER');
+    $password = getenv('PGPASSWORD');
 
-//$db = pg_connect("host=localhost port=5432 dbname=tuvet user=adolfo password=ofloda01");
+    if (!$host || !$port || !$dbname || !$user || !$password) {
+        die('Faltan variables de entorno');
+    }
 
+    $db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-
-$host = getenv('PGHOST');
-$port = getenv('PGPORT');
-$dbname = getenv('PGDATABASE');
-$user = getenv('PGUSER');
-$password = getenv('PGPASSWORD');
-
-if (!$host || !$port || !$dbname || !$user || !$password) {
-    die("Faltan variables de entorno para la conexión a la base de datos.");
+    if (!$db) {
+        die('Error de conexión: ' . pg_last_error());
+    }
 }
-
-$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-
-if (!$db) {
-    die("No se pudo conectar a la base de datos.");
-}
-
-
-
 ?>
-
-
-
-
-
-
-
 
