ae63a7077a6a5f9a16bc53faa4e8b3c79b68e596
montaniaRoja <adolfocp1972@gmail.com>
2025-01-22 13:50:44 -0600

    variables de entorno

--- vms/admin/include/config.php
+++ vms/admin/include/config.php
@@ -1,18 +1,39 @@
 <?php
+function loadEnv($file)
+{
+    if (!file_exists($file)) {
+        die("Archivo .env no encontrado");
+    }
+
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue; // Ignorar comentarios
+        }
+        list($key, $value) = explode('=', $line, 2);
+        putenv(trim($key) . '=' . trim($value));
+    }
+}
+
+// Cargar variables de entorno
+loadEnv(__DIR__ . '/.env');
+
 $ip = $_SERVER['REMOTE_ADDR'];
 $ip_locales = array('127.0.0.1', '::1');
 $is_localhost = in_array($ip, $ip_locales);
 
 if ($is_localhost) {
-    // Para entorno local
     $db = pg_connect("host=localhost port=5432 dbname=tuvet user=adolfo password=ofloda01");
 } else {
-    // Para entorno remoto (Google Cloud SQL o IP pública)
-    $host = "34.122.70.129";
-    $port = "5432";
-    $dbname = "tuvetdatabase";
-    $user = "postgres";
-    $password = "ofloda01";
+    $host = getenv('PGHOST');
+    $port = getenv('PGPORT');
+    $dbname = getenv('PGDATABASE');
+    $user = getenv('PGUSER');
+    $password = getenv('PGPASSWORD');
+
+    if (!$host || !$port || !$dbname || !$user || !$password) {
+        die('Faltan variables de entorno');
+    }
 
     $db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
@@ -20,4 +41,3 @@ if ($is_localhost) {
         die('Error de conexión: ' . pg_last_error());
     }
 }
-?>
--- vms/atencionalcliente/include/config.php
+++ vms/atencionalcliente/include/config.php
@@ -1,13 +1,30 @@
 <?php
+function loadEnv($file)
+{
+    if (!file_exists($file)) {
+        die("Archivo .env no encontrado");
+    }
+
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue; // Ignorar comentarios
+        }
+        list($key, $value) = explode('=', $line, 2);
+        putenv(trim($key) . '=' . trim($value));
+    }
+}
+
+// Cargar variables de entorno
+loadEnv(__DIR__ . '/.env');
+
 $ip = $_SERVER['REMOTE_ADDR'];
 $ip_locales = array('127.0.0.1', '::1');
 $is_localhost = in_array($ip, $ip_locales);
 
 if ($is_localhost) {
-    // Para entorno local
     $db = pg_connect("host=localhost port=5432 dbname=tuvet user=adolfo password=ofloda01");
 } else {
-    // Para entorno remoto (Google Cloud SQL o IP pública)
     $host = getenv('PGHOST');
     $port = getenv('PGPORT');
     $dbname = getenv('PGDATABASE');
@@ -24,5 +41,3 @@ if ($is_localhost) {
         die('Error de conexión: ' . pg_last_error());
     }
 }
-?>
-
